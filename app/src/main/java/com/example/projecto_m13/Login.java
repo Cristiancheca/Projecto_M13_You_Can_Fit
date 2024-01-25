1818152d9ce93e089befd4fb91f8517581641e4b
StivenFG <stivenfg2001@gmail.com>
2024-01-25 10:56:18 +0100

    Eliminacion de main Activity y splash screen

--- app/src/main/java/com/example/projecto_m13/Login.java
+++ app/src/main/java/com/example/projecto_m13/Login.java
@@ -8,6 +8,12 @@ public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(1000); // hacemos que el splash screen tarde 1 segundo en cargar
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        setTheme(R.style.Base_Theme_Projecto_M13); // Asignacion de splashScreen antes en de entrar al login
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
