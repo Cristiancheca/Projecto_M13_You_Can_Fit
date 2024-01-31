823eff2e9b1bd6271fbc555d3bf6db1e824fae5f
jofa446 <JOFA446@vidalibarraquer.net>
2024-01-31 08:50:32 +0100

    a

---
+++ app/src/main/java/com/example/projecto_m13/Ejercicios_dia.java
@@ -0,0 +1,14 @@
+package com.example.projecto_m13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ejercicios_dia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicios_dia);
+    }
+}
